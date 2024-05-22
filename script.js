7f46551f03165b51bc599ae0b0365619de001869
Amy Au <aa4644@drexel.edu>
2024-05-22 00:32:38 -0400

    slime pop-up

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
+//help button
 const open = document.getElementById('help_button');
 const help_modal_container = document.getElementById('help_modal_container');
 const close = document.getElementById('close');
 
 open.addEventListener('click', () => {
-  help_modal_container.classList.add('show');
+    help_modal_container.classList.add('show');
 });
 
 close.addEventListener('click', () => {
-  help_modal_container.classList.remove('show');
+    help_modal_container.classList.remove('show');
+});
+
+// puddle slime
+const puddleSlimeButton = document.getElementById('puddle');
+const slimePopupOverlay = document.getElementById('slime-PopUp');
+const closePopupButton = document.getElementById('close-popup');
+
+puddleSlimeButton.addEventListener('click', () => {
+    slimePopupOverlay.classList.add('show');
+});
+
+
+closePopupButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    slimePopupOverlay.classList.remove('show');
 });
