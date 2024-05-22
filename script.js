e19e4e7e7a342d250257ea7c28552d20620798dc
Amy Au <aa4644@drexel.edu>
2024-05-22 00:19:19 -0400

    help button pop-up

---
+++ script.js
@@ -0,0 +1,11 @@
+const open = document.getElementById('help_button');
+const help_modal_container = document.getElementById('help_modal_container');
+const close = document.getElementById('close');
+
+open.addEventListener('click', () => {
+  help_modal_container.classList.add('show');
+});
+
+close.addEventListener('click', () => {
+  help_modal_container.classList.remove('show');
+});
